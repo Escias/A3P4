9f83843ebab8b11db6f63493fbe0fb570fd41a87
Jimmy Lai <ljimmy98@hotmail.fr>
2020-01-13 11:59:31 +0100

    All Request

--- src/main/java/org/projet4/javadomo/Sensor.java
+++ src/main/java/org/projet4/javadomo/Sensor.java
@@ -5,14 +5,37 @@ import java.sql.*;
 
 public class Sensor {
     JPanel pscroll = new JPanel();
+    Box l1 = Box.createHorizontalBox();
+    Box l2 = Box.createHorizontalBox();
+    Box l3 = Box.createHorizontalBox();
+    Box l4 = Box.createHorizontalBox();
+    Box l5 = Box.createHorizontalBox();
+    Box l6 = Box.createHorizontalBox();
+    Box c1 = Box.createVerticalBox();
 
-    public void Sensor(JFrame window, int id) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/projet4", "root", "root");
-        String request = "SELECT room_name, room_description FROM room WHERE room_user_id = "+id+" ORDER BY room_name ASC;";
-        Statement stm = connection.createStatement();
+    public void Sensor(JFrame window, int id, Connection co) throws SQLException {
+        String request = "SELECT sensor_id, sensor_name, R.room_name, sensor_status, sensor_interval, sensor_temp_min, sensor_temp_max " +
+                "FROM sensor AS S " +
+                "LEFT JOIN room AS R " +
+                "ON R.room_id = S.sensor_room_id " +
+                "WHERE R.room_user_id = " + id +
+                " ORDER BY sensor_id ASC;";
+        Statement stm = co.createStatement();
         ResultSet rslt = stm.executeQuery(request);
         while (rslt.next()){
-            pscroll.add(new JLabel());
+            l1.add(new JLabel("Name : " + rslt.getString(2)));
+            l2.add(new JLabel("Salle : " + rslt.getString(3)));
+            l3.add(new JLabel("Etat : " + rslt.getString(4)));
+            l4.add(new JLabel("Interval : " + rslt.getInt(5)));
+            l5.add(new JLabel("Température min : " + rslt.getInt(6)));
+            l6.add(new JLabel("Température max : " + rslt.getInt(7)));
+            c1.add(l1);
+            c1.add(l2);
+            c1.add(l3);
+            c1.add(l4);
+            c1.add(l5);
+            c1.add(l6);
+            pscroll.add(c1);
             window.getContentPane().add(pscroll);
             window.setVisible(true);
         }
--- src/main/java/org/projet4/javadomo/ThermoIntel.java
+++ src/main/java/org/projet4/javadomo/ThermoIntel.java
@@ -5,14 +5,41 @@ import java.sql.*;
 
 public class ThermoIntel {
     JPanel pscroll = new JPanel();
+    Box l1 = Box.createHorizontalBox();
+    Box l2 = Box.createHorizontalBox();
+    Box l3 = Box.createHorizontalBox();
+    Box l4 = Box.createHorizontalBox();
+    Box l5 = Box.createHorizontalBox();
+    Box l6 = Box.createHorizontalBox();
+    Box c1 = Box.createVerticalBox();
 
-    public void ThermoIntel(JFrame window, int id) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/projet4", "root", "root");
-        String request = "SELECT room_name, room_description FROM room WHERE room_user_id = "+id+" ORDER BY room_name ASC;";
-        Statement stm = connection.createStatement();
+    public void ThermoIntel(JFrame window, int id, Connection co) throws SQLException {
+        String request = "SELECT thermo_id, R.room_name, thermo_name, thermo_temp_target, thermo_status, " +
+                "(SELECT sensor_name FROM sensor as S WHERE S.sensor_id = T.thermo_id_1) as nom_1, " +
+                "(SELECT sensor_name FROM sensor as S WHERE S.sensor_id = T.thermo_id_2) as nom_2 " +
+                "FROM thermointel as T " +
+                "LEFT JOIN sensor as S " +
+                "ON T.thermo_name = S.sensor_name " +
+                "LEFT JOIN room as R " +
+                "ON R.room_id = T.thermo_room_id " +
+                "WHERE R.room_user_id = " + id +
+                "ORDER BY thermo_id ASC;";
+        Statement stm = co.createStatement();
         ResultSet rslt = stm.executeQuery(request);
         while (rslt.next()){
-            pscroll.add(new JLabel());
+            l1.add(new JLabel("Name : " + rslt.getString(3)));
+            l2.add(new JLabel("Salle : " + rslt.getString(2)));
+            l3.add(new JLabel("Température cible : " + rslt.getString(4)));
+            l4.add(new JLabel("Etat : " + rslt.getInt(5)));
+            l5.add(new JLabel("Capteur 1 : " + rslt.getInt(6)));
+            l6.add(new JLabel("Capteur 2 : " + rslt.getInt(7)));
+            c1.add(l1);
+            c1.add(l2);
+            c1.add(l3);
+            c1.add(l4);
+            c1.add(l5);
+            c1.add(l6);
+            pscroll.add(c1);
             window.getContentPane().add(pscroll);
             window.setVisible(true);
         }
